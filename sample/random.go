466bf9f050d85de38eb70f9751aaf4599c0c37f3
faa678 <2532506036@qq.com>
2021-06-27 17:01:29 +0800

    protobuf try in go

---
+++ sample/random.go
@@ -0,0 +1,127 @@
+package sample
+
+import (
+	"math/rand"
+	"time"
+
+	"github.com/google/uuid"
+
+	pb "github.com/techschool/pcbook/pb"
+)
+
+func init() {
+	rand.Seed(time.Now().UnixNano())
+}
+
+func randomKeyboardLayout() pb.Keyboard_Layout {
+	switch rand.Intn(3) {
+	case 1:
+		return pb.Keyboard_QWERTY
+	case 2:
+		return pb.Keyboard_QWERTZ
+	default:
+		return pb.Keyboard_AZERTY
+	}
+}
+
+func randomCPUBrand() string {
+	return randomStringFromSet("Intel", "AMD")
+}
+
+func randomCPUName(brand string) string {
+	if brand == "intel" {
+		return randomStringFromSet(
+			"Xeon E",
+			"Core i9",
+			"Core i7",
+			"Core i5",
+			"Core i3",
+		)
+	}
+	return randomStringFromSet(
+		"Ryzen 7",
+		"Ryzen 5",
+		"Ryzen 3",
+	)
+}
+
+func randomGPUBrand() string {
+	return randomStringFromSet("NVIDIA", "AMD")
+}
+
+func randomGPUName(brand string) string {
+	if brand == "NVIDIA" {
+		return randomStringFromSet(
+			"RTX 2060",
+			"RTX 2070",
+			"GTX 1660-Ti",
+			"GTX 1070",
+		)
+	}
+	return randomStringFromSet(
+		"RX 590",
+		"RX 580",
+		"RX 5700-XT",
+		"RX Vega-56",
+	)
+}
+
+func randomLaptopBrand() string {
+	return randomStringFromSet("Apple", "Dell", "Lenovo")
+}
+
+func randomLaptopName(brand string) string {
+	switch brand {
+	case "Apple":
+		return randomStringFromSet("Macbook Air", "Macbook Pro")
+	case "Dell":
+		return randomStringFromSet("Latitude", "Vostro", "XPS", "Alienware")
+	default:
+		return randomStringFromSet("Thinkpad X1", "Thinkpad P1", "Thinkpad P53")
+	}
+}
+
+func randomScreenResolution() *pb.Screen_Resolution {
+	height := randomInt(1080, 4320)
+	width := height * 16 / 9
+	resolution := &pb.Screen_Resolution{
+		Height: uint32(height),
+		Width:  uint32(width),
+	}
+	return resolution
+}
+
+func randomScreenPanel() pb.Screen_Panel {
+	if rand.Intn(2) == 1 {
+		return pb.Screen_IPS
+	}
+	return pb.Screen_OLED
+}
+
+func randomStringFromSet(a ...string) string {
+	n := len(a)
+	if n == 0 {
+		return ""
+	}
+	return a[rand.Intn(n)]
+}
+
+func randomInt(min int, max int) int {
+	return min + rand.Intn(max-min)
+}
+
+func randomFloat64(min float64, max float64) float64 {
+	return min + rand.Float64()*(max-min)
+}
+
+func randomFloa32(min float32, max float32) float32 {
+	return min + rand.Float32()*(max-min)
+}
+
+func randomBool() bool {
+	return rand.Intn(2) == 1
+}
+
+func randomId() string {
+	return uuid.New().String()
+}
